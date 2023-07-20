aba545fd2f008a7168fd26b8e34e3d9591f9ede3
Ruslan Simonenko <ruslan.simonenko@gmail.com>
2023-07-20 01:37:12 +0100

    client: fixed types in api-client

--- client/src/api/api-client-axios.ts
+++ client/src/api/api-client-axios.ts
@@ -1,4 +1,4 @@
-import type {App} from 'vue'
+import type {App, Plugin} from 'vue'
 import {inject} from "vue";
 
 import axios, {AxiosInstance} from "axios";
@@ -9,10 +9,10 @@ interface Options {
 
 const apiClientAxiosKey = Symbol('apiClientAxios')
 
-export const useApiClientAxios: () => AxiosInstance = () => inject(apiClientAxiosKey)
+export const useApiClientAxios: () => AxiosInstance = () => inject(apiClientAxiosKey)!
 
 // noinspection JSUnusedGlobalSymbols
-export const ApiClientAxios = {
+export const ApiClientAxios: Plugin = {
   install: (app: App, _options: Options) => {
     const apiClientAxios = axios.create({
       baseURL: "/api",
