fecc9564e1a8f7a0ee1b6fa98e70a610a27c3f91
Ruslan Simonenko <ruslan.simonenko@gmail.com>
2023-07-20 23:04:33 +0100

    client: moved user info button to its own component

--- client/src/navigation/Header.vue
+++ client/src/navigation/Header.vue
@@ -2,37 +2,11 @@
   <q-header elevated>
     <q-toolbar>
       <q-toolbar-title>STC Ad Credit</q-toolbar-title>
-      <q-btn-dropdown v-if="authStore.isAuthenticated"
-                      flat>
-        <template v-slot:label>
-          <q-avatar>
-            <img :src="userPictureUrl"/>
-          </q-avatar>
-        </template>
-        <q-list>
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar>
-                <img :src="userPictureUrl"/>
-              </q-avatar>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>{{ userName }}<br>{{ userEmail }}</q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-btn-dropdown>
+      <UserInfoButton></UserInfoButton>
     </q-toolbar>
   </q-header>
 </template>
 
 <script setup lang="ts">
-import {useAuthStore} from "../auth/auth-store.ts";
-import {computed} from "vue";
-
-const authStore = useAuthStore();
-const userName = computed(() => authStore.user!.name)
-const userEmail = computed(() => authStore.user!.email)
-const userPictureUrl = computed(() => authStore.user!.picture_url)
-
+import UserInfoButton from "./UserInfoButton.vue";
 </script>
