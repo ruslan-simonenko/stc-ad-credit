9fee3254e489e6e956be7103286bdeb799e4dd23
Ruslan Simonenko <ruslan.simonenko@gmail.com>
2023-07-21 00:27:34 +0100

    client: routing guards for /home and /login

--- client/src/navigation/routing.ts
+++ client/src/navigation/routing.ts
@@ -1,11 +1,29 @@
 import {createRouter, createWebHashHistory, RouteRecordRaw} from "vue-router";
 import LoginPage from "../auth/LoginPage.vue";
 import AdminPage from "../app/admin/AdminPage.vue";
+import {useAuthStore} from "../auth/auth-store.ts";
+
+const navigateToHomeIfAuthenticated = () => {
+    const authStore = useAuthStore();
+    if (authStore.isAuthenticated) {
+        return {name: 'Admin'}
+    }
+    return true
+}
+
+const redirectFromHome = () => {
+    const authStore = useAuthStore()
+    if (authStore.isAuthenticated) {
+        return {name: 'Admin'}
+    } else {
+        return {name: 'Login'}
+    }
+}
 
 const routes: RouteRecordRaw[] = [
+    {name: 'Home', path: '/', redirect: redirectFromHome},
+    {name: "Login", path: '/login', beforeEnter: navigateToHomeIfAuthenticated, component: LoginPage},
     {name: 'Admin', path: '/admin', component: AdminPage},
-    {name: "Login", path: '/login', component: LoginPage},
-    {name: 'Home', path: '/', redirect: {name: 'Login'}},
 ]
 
 export const appRouter = createRouter({
