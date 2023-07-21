2297e869f3f85a429c52d0a338c10cd185ba757d
Ruslan Simonenko <ruslan.simonenko@gmail.com>
2023-07-21 01:01:56 +0100

    client: redirect to login if on a page that requires authentication

--- client/src/auth/LoginPage.vue
+++ client/src/auth/LoginPage.vue
@@ -7,13 +7,20 @@
 <script setup lang="ts">
 import {CallbackTypes, GoogleLogin} from "vue3-google-login"
 import {useAuthStore} from "./auth-store.ts";
-import {useRouter} from "vue-router";
+import {useRoute, useRouter} from "vue-router";
 
 const authStore = useAuthStore()
 const router = useRouter()
+const route = useRoute()
 
 const callback: CallbackTypes.CredentialCallback = async (response: CallbackTypes.CredentialPopupResponse) => {
   await authStore.loginWithGoogle(response.credential)
-      .then(() => router.push({name: "Home"}))
+      .then(() => {
+        if ('next' in route.query) {
+          router.push(route.query['next'] as string)
+        } else {
+          router.push({name: "Home"})
+        }
+      })
 }
 </script>
--- client/src/navigation/routing.ts
+++ client/src/navigation/routing.ts
@@ -23,11 +23,20 @@ const redirectFromHome = () => {
 const routes: RouteRecordRaw[] = [
     {name: 'Home', path: '/', redirect: redirectFromHome},
     {name: "Login", path: '/login', beforeEnter: navigateToHomeIfAuthenticated, component: LoginPage},
-    {name: 'Admin', path: '/admin', component: AdminPage},
+    {name: 'Admin', path: '/admin', component: AdminPage, meta: {requiresAuth: true}},
 ]
 
 export const appRouter = createRouter({
     history: createWebHashHistory(),
     routes
 })
-
+appRouter.beforeEach((to) => {
+    const authStore = useAuthStore()
+    if (to.meta.requiresAuth !== true || authStore.isAuthenticated) {
+        return true;
+    }
+    return {
+        name: 'Login',
+        query: {next: to.fullPath}
+    }
+})
