8d85dea02da3196dde3110b5273c1b4098bca861
Ruslan Simonenko <ruslan.simonenko@gmail.com>
2023-07-21 22:44:30 +0100

    client: Added mock list of Carbon Auditors

--- client/src/app/admin/AdminPage.vue
+++ client/src/app/admin/AdminPage.vue
@@ -1,9 +1,10 @@
 <template>
   <q-page padding>
     Admin page
+    <CarbonAuditorsList></CarbonAuditorsList>
   </q-page>
 </template>
 
 <script setup lang="ts">
-
+import CarbonAuditorsList from "../carbon_auditor/CarbonAuditorsList.vue";
 </script>
---
+++ client/src/app/carbon_auditor/CarbonAuditorsList.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-list bordered padding>
+    <q-item-label header>Carbon Auditors</q-item-label>
+    <q-item v-for="auditor in auditors">
+      <q-item-section avatar>
+        <q-avatar><img :src="auditor.picture_url"></q-avatar>
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ auditor.name }}</q-item-label>
+        <q-item-label>{{ auditor.email }}</q-item-label>
+      </q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script setup lang="ts">
+const auditors = [
+  {picture_url: 'https://cdn.quasar.dev/img/avatar4.jpg', name: 'John Doe', email: 'john.doe@gmail.com'},
+  {picture_url: 'https://cdn.quasar.dev/img/avatar2.jpg', name: 'Jane Doe', email: 'jane.doe@gmail.com'}
+]
+</script>
