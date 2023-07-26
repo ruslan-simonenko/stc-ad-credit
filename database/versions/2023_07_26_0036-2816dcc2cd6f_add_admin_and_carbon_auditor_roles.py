e14f2ec813bffe07f29a0a1c7b21aaf1d0be7617
Ruslan Simonenko <ruslan.simonenko@gmail.com>
2023-07-26 01:42:06 +0100

    database: added admin and carbon auditor roles

---
+++ database/versions/2023_07_26_0036-2816dcc2cd6f_add_admin_and_carbon_auditor_roles.py
@@ -0,0 +1,31 @@
+"""add admin and carbon auditor roles
+
+Revision ID: 2816dcc2cd6f
+Revises: 3f0cd063bfdd
+Create Date: 2023-07-26 00:36:09.658477+00:00
+
+"""
+from typing import Any
+
+from alembic import op
+from sqlalchemy import MetaData, Table
+
+# revision identifiers, used by Alembic.
+revision = '2816dcc2cd6f'
+down_revision = '3f0cd063bfdd'
+branch_labels = None
+depends_on = None
+
+
+def upgrade(is_dev: bool, **kw: Any) -> None:
+    meta = MetaData()
+    meta.reflect(bind=op.get_bind(), only=('role',))
+    op.bulk_insert(Table('role', meta),
+                   [{'name': 'Admin', },
+                    {'name': 'Carbon Auditor', }])
+
+
+def downgrade(is_dev: bool, **kw: Any) -> None:
+    meta = MetaData()
+    meta.reflect(bind=op.get_bind(), only=('role',))
+    op.execute("DELETE FROM role WHERE name IN ('Admin', 'Carbon Auditor')")
