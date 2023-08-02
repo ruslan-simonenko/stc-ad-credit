4c35e528bdd5c1fa71482dd6517f87659ec77983
Ruslan Simonenko <ruslan.simonenko@gmail.com>
2023-08-02 01:06:17 +0100

    client: added form scaffolding to add businesses

---
+++ client/src/app/business/BusinessAddForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-form @submit="onSubmit" class="q-gutter-md">
+    <q-input
+        type="text"
+        v-model="name"
+        label="Name"
+    />
+    <q-input
+        type="url"
+        v-model="facebookLink"
+        label="Facebook Link"
+    />
+    <q-btn label="Add Business" type="submit" color="primary"/>
+  </q-form>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const name = ref<string>('')
+const facebookLink = ref<string>('')
+
+const onSubmit = () => {
+  console.log(`Add business: ${name.value} at ${facebookLink.value}`)
+}
+</script>
+
+<style scoped>
+
+</style>
--- client/src/app/business/BusinessesPage.vue
+++ client/src/app/business/BusinessesPage.vue
@@ -1,9 +1,12 @@
 <template>
-  List of businesses
+  <q-page padding>
+    <BusinessAddForm/>
+  </q-page>
 </template>
 
 <script setup lang="ts">
 
+import BusinessAddForm from "./BusinessAddForm.vue";
 </script>
 
 <style scoped>
