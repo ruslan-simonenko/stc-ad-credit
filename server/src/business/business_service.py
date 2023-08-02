1f4c661c45c22d03cc2cf87628ae5b4af927cdbe
Ruslan Simonenko <ruslan.simonenko@gmail.com>
2023-08-02 18:46:13 +0100

    server: business service - use user id instead of user

--- server/src/business/business_service.py
+++ server/src/business/business_service.py
@@ -6,16 +6,15 @@ from sqlalchemy.exc import IntegrityError
 
 from src.persistence.schema import db
 from src.persistence.schema.business import Business
-from src.persistence.schema.user import User
 
 
 class BusinessService:
     @staticmethod
-    def add(name: str, facebook_url: Optional[str], creator: User) -> Business:
+    def add(name: str, facebook_url: Optional[str], creator_id: int) -> Business:
         business = Business(
             name=name,
             facebook_url=facebook_url,
-            created_by=creator.id,
+            created_by=creator_id,
             created_at=datetime.utcnow()
         )
         db.session.add(business)
--- server/tests/business/business_service_test.py
+++ server/tests/business/business_service_test.py
@@ -25,21 +25,20 @@ class TestBusinessService(DatabaseTest):
         ('test business', 'https://www.facebook.com/groups/seethroughnews/'),
     ])
     def test_add_business(self, current_user, name, facebook_url):
-        business = BusinessService.add(name=name, facebook_url=facebook_url, creator=current_user)
+        business = BusinessService.add(name=name, facebook_url=facebook_url, creator_id=current_user.id)
         assert business.name == name
         assert business.facebook_url == facebook_url
         assert business.created_by == current_user.id
         assert datetime.utcnow() - business.created_at < timedelta(minutes=1)
 
     def test_add_business_duplicate_name(self, current_user):
-        BusinessService.add(name='test business', facebook_url=None, creator=current_user)
+        BusinessService.add(name='test business', facebook_url=None, creator_id=current_user.id)
         with pytest.raises(ValueError, match='Business name is already in use'):
-            BusinessService.add(name='test business', facebook_url=None, creator=current_user)
+            BusinessService.add(name='test business', facebook_url=None, creator_id=current_user.id)
 
     def test_get_all_businesses(self, current_user):
-        biz_a = BusinessService.add('test biz a', facebook_url=None, creator=current_user)
-        biz_b = BusinessService.add('test biz b', facebook_url='https://facebook.com/somepage', creator=current_user)
-        biz_c = BusinessService.add('test biz c', facebook_url=None, creator=current_user)
+        biz_a = BusinessService.add('test biz a', facebook_url=None, creator_id=current_user.id)
+        biz_b = BusinessService.add('test biz b', facebook_url='https://facebook.com/apage', creator_id=current_user.id)
+        biz_c = BusinessService.add('test biz c', facebook_url=None, creator_id=current_user.id)
         actual_businesses = BusinessService.get_all()
         assert set(actual_businesses) == {biz_a, biz_b, biz_c}
-
