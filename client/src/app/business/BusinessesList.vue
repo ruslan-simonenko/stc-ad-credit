e55c4de02f428fc6fe8ef6e64661b2fc0350f50c
Ruslan Simonenko <ruslan.simonenko@gmail.com>
2023-08-05 13:41:11 +0100

    client: added carbon audit score to businesses table

--- client/src/app/business/BusinessesList.vue
+++ client/src/app/business/BusinessesList.vue
@@ -1,64 +1,47 @@
 <template class="relative-position">
-  <q-list>
-    <q-item>
-      <BusinessAddForm/>
-    </q-item>
-    <!-- Loaded -->
-    <template v-if="state == State.LOADED">
-      <template v-for="(business, index) in businessStore.all.items">
-        <q-separator v-if="index !== 0" spaced inset/>
-        <q-item>
-          <q-item-section>
-            <q-item-label>{{ business.name }}</q-item-label>
-            <q-item-label v-if="business.facebook_url">{{ business.facebook_url }}</q-item-label>
-          </q-item-section>
-        </q-item>
-      </template>
-    </template>
-    <!-- Loading -->
-    <q-inner-loading :showing="state == State.LOADING" label="Please wait..."/>
-    <!-- Error -->
-    <q-item v-if="state == State.ERROR">
-      <q-item-section class="error-background"></q-item-section>
-    </q-item>
-    <q-inner-loading :showing="state == State.ERROR">
-      <div class="column items-center">
-        <q-icon name="warning" color="warning" size="4rem"></q-icon>
-        <span class="text-dark">Can't load the data</span>
-        <q-btn flat color="primary" label="Try again" @click="businessStore.fetch()"></q-btn>
-      </div>
-    </q-inner-loading>
-  </q-list>
+  <q-table
+      flat bordered
+      title="Businesses"
+      :rows="businessStore.all.items"
+      :columns="columns"
+      row-key="id"
+  />
 </template>
 
 <script setup lang="ts">
 import {useBusinessStore} from "./business-store.ts";
-import BusinessAddForm from "./BusinessAddForm.vue";
-import {computed, onMounted} from "vue";
+import {onMounted} from "vue";
+import {useCarbonAuditStore} from "../carbon-audit/carbon-audit-store.ts";
+import {Business} from "./business-types.ts";
 
 const businessStore = useBusinessStore()
+const auditStore = useCarbonAuditStore();
+
+const columns = [
+  {
+    name: 'business',
+    label: 'Business',
+    align: 'left',
+    field: (row: Business) => row.name,
+  },
+  {
+    name: 'score',
+    label: 'Carbon Audit Score',
+    field: (row: Business) => auditStore.all.items.find(audit => audit.business_id === row.id)?.score,
+  },
+  {
+    name: 'facebook_url',
+    label: 'Facebook URL',
+    align: 'left',
+    field: (row: Business) => row.facebook_url,
+  }
+]
 
 onMounted(() => {
   businessStore.fetch()
-});
-
-enum State {
-  LOADED, LOADING, ERROR
-}
-
-const state = computed<State>(() => {
-  if (businessStore.all.fetching) {
-    return State.LOADING
-  }
-  if (businessStore.all.error) {
-    return State.ERROR
-  }
-  return State.LOADED
+  auditStore.fetch()
 });
 </script>
 
 <style scoped>
-.error-background {
-  height: 10rem
-}
 </style>
--- client/src/app/business/BusinessesPage.vue
+++ client/src/app/business/BusinessesPage.vue
@@ -1,11 +1,13 @@
 <template>
   <q-page padding>
+    <BusinessAddForm/>
     <BusinessesList/>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import BusinessesList from "./BusinessesList.vue";
+import BusinessAddForm from "./BusinessAddForm.vue";
 </script>
 
 <style scoped>
