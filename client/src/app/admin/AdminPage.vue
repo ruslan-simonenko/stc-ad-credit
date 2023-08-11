7e49792db046155745496bbc587950f03f8ad3be
Ruslan Simonenko <ruslan.simonenko@gmail.com>
2023-08-11 18:12:57 +0100

    client: added new users table foundation

--- client/src/app/admin/AdminPage.vue
+++ client/src/app/admin/AdminPage.vue
@@ -1,9 +1,11 @@
 <template>
   <q-page padding>
     <UserManagementList></UserManagementList>
+    <UsersTable></UsersTable>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import UserManagementList from "../../user/management/UserManagementList.vue";
+import UsersTable from "../../user/management/UsersTable.vue";
 </script>
---
+++ client/src/user/management/UsersTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-table
+      title="Users"
+      :rows="userStore.all.items"
+      :columns="columns"
+      :loading="userStore.all.fetching"
+  />
+</template>
+
+<script setup lang="ts">
+import {useUserStore} from "../user-store.ts";
+import {User} from "../user.ts";
+import {onMounted} from "vue";
+
+const userStore = useUserStore();
+
+const columns = [
+  {name: 'name', label: 'Name', align: 'left', field: (user: User) => user.name},
+  {name: 'email', label: 'E-mail', align: 'left', field: (user: User) => user.email},
+  {name: 'roles',label: 'Roles', align: 'left', field: (user: User) => user.roles},
+  {name: 'actions', },
+]
+
+onMounted(() => {
+  userStore.fetch()
+});
+</script>
+
+<style scoped>
+
+</style>
