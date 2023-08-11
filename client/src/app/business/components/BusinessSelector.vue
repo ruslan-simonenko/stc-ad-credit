0a4b15fec15853e79c1d0e3ea42661c6344033e0
Ruslan Simonenko <ruslan.simonenko@gmail.com>
2023-08-11 20:24:00 +0100

    client: extracted BusinessSelector from CarbonAuditAddForm

---
+++ client/src/app/business/components/BusinessSelector.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-select label="Business"
+            v-model="business"
+            :loading="businessStore.all.fetching"
+            :options="businessStore.all.items"
+            :rules="businessValidators">
+    <template v-slot:option="{ itemProps, opt }: {opt: Business}">
+      <q-item v-bind="itemProps">
+        <q-item-section>
+          <q-item-label>{{ opt.name }}</q-item-label>
+          <q-item-label>{{ opt.facebook_url }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </template>
+    <template v-slot:selected-item="{ opt }: {opt: Business}">
+      {{ opt.name }}
+    </template>
+    <template v-slot:no-option>
+      <q-item>
+        <q-item-section class="text-grey">
+          No results
+        </q-item-section>
+      </q-item>
+    </template>
+  </q-select>
+</template>
+
+<script setup lang="ts">
+import {Business} from "../business-types.ts";
+import {computed, onMounted, ref} from "vue";
+import {useBusinessStore} from "../business-store.ts";
+import {fieldRequiredValidator} from "../../../utils/form-validators.ts";
+
+const props = defineProps(['modelValue'])
+const emit = defineEmits(['update:modelValue'])
+
+const business = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value: Business) {
+    emit('update:modelValue', value)
+  }
+})
+
+const businessValidators = [fieldRequiredValidator]
+
+const businessStore = useBusinessStore()
+
+onMounted(() => {
+  businessStore.fetch()
+});
+</script>
--- client/src/app/carbon-audit/CarbonAuditAddForm.vue
+++ client/src/app/carbon-audit/CarbonAuditAddForm.vue
@@ -1,29 +1,6 @@
 <template>
   <q-form @submit="onSubmit" class="q-gutter-md form">
-    <q-select label="Business"
-              v-model="business"
-              :loading="businessStore.all.fetching"
-              :options="businessStore.all.items"
-              :rules="businessValidators">
-      <template v-slot:option="{ itemProps, opt }: {opt: Business}">
-        <q-item v-bind="itemProps">
-          <q-item-section>
-            <q-item-label>{{ opt.name }}</q-item-label>
-            <q-item-label>{{ opt.facebook_url }}</q-item-label>
-          </q-item-section>
-        </q-item>
-      </template>
-      <template v-slot:selected-item="{ opt }: {opt: Business}">
-        {{ opt.name }}
-      </template>
-      <template v-slot:no-option>
-        <q-item>
-          <q-item-section class="text-grey">
-            No results
-          </q-item-section>
-        </q-item>
-      </template>
-    </q-select>
+    <BusinessSelector v-model="business"/>
     <q-input label="Score [0-100]" v-model="score" type="number" :rules="scoreValidators"/>
     <q-input label="Date" v-model="reportDate" type="date" :rules="reportDateValidators"/>
     <q-input label="Report URL" v-model="reportUrl" type="url" :rules="reportUrlValidators"/>
@@ -32,18 +9,17 @@
 </template>
 
 <script setup lang="ts">
-import {useBusinessStore} from "../business/business-store.ts";
-import {onMounted, ref} from "vue";
+import {ref} from "vue";
 import {Business} from "../business/business-types.ts";
 import {useCarbonAuditStore} from "./carbon-audit-store.ts";
+import BusinessSelector from "../business/components/BusinessSelector.vue";
 import {fieldRequiredValidator} from "../../utils/form-validators.ts";
 
-const businessStore = useBusinessStore();
 const carbonAuditStore = useCarbonAuditStore();
 
 const business = ref<Business | null>(null)
-const businessValidators = [fieldRequiredValidator]
 const score = ref<number | null>(null)
+
 const scoreValidators = [
   fieldRequiredValidator,
   (value: number) => value >= 0 || 'Must be greater or equal to 0',
@@ -56,10 +32,6 @@ const reportDateValidators = [
 ]
 const reportUrl = ref<string | null>(null)
 const reportUrlValidators = [fieldRequiredValidator]
-
-onMounted(() => {
-  businessStore.fetch()
-});
 
 const onSubmit = async () => {
   await carbonAuditStore.add({
