66bdd90ab4ddf6f46aa50bbf1bf74d261c6dc4af
Ruslan Simonenko <ruslan.simonenko@gmail.com>
2023-08-13 20:52:42 +0100

    server: ad record service - added function to get count of ad records for a business since a specific time

--- server/src/ad_record/ad_record_service.py
+++ server/src/ad_record/ad_record_service.py
@@ -1,10 +1,12 @@
 from datetime import datetime, date
 from typing import List
 
-from sqlalchemy import select
+from sqlalchemy import select, and_
+from sqlalchemy.sql.functions import count
 
 from src.persistence.schema import db
 from src.persistence.schema.ad_record import AdRecord
+from src.utils.clock import Clock
 
 
 class AdRecordService:
@@ -15,7 +17,7 @@ class AdRecordService:
             business_id=business_id,
             ad_post_url=ad_post_url,
             created_by=creator_id,
-            created_at=datetime.utcnow()
+            created_at=Clock.now()
         )
         db.session.add(ad_record)
         db.session.commit()
@@ -25,3 +27,10 @@ class AdRecordService:
     def get_all() -> List[AdRecord]:
         query = select(AdRecord).order_by(AdRecord.created_at.desc())
         return db.session.execute(query).scalars().all()
+
+    @staticmethod
+    def get_count_for_business_since_date(business_id: int, since: datetime) -> int:
+        query = select(count(AdRecord.id)) \
+            .where(and_(AdRecord.business_id == business_id,
+                        AdRecord.created_at >= since))
+        return db.session.execute(query).scalar_one()
--- server/tests/ad_record/ad_record_service_test.py
+++ server/tests/ad_record/ad_record_service_test.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from typing import List
 
 import pytest
+from _pytest.monkeypatch import MonkeyPatch
 
 from app import app
 from src.ad_record.ad_record_service import AdRecordService
@@ -11,6 +12,7 @@ from src.persistence.schema.business import Business
 from src.persistence.schema.user import User
 from src.user.user_service import UserService
 from src.user.user_types import UserRole
+from src.utils.clock import Clock
 from tests.persistence.db_test import DatabaseTest
 
 
@@ -69,3 +71,61 @@ class TestAdRecordService(DatabaseTest):
         def test_get_all(self, ad_records: List[AdRecord]):
             actual_ad_records = AdRecordService.get_all()
             assert actual_ad_records == list(reversed(ad_records))
+
+    class TestGetCountForBusinessSinceDate:
+
+        @pytest.fixture(autouse=True)
+        def ad_records(self, user_ad_manager: User, business: Business, monkeypatch: MonkeyPatch):
+            current_time = datetime.utcnow()
+            for days_ago in [120, 100, 90, 70, 50, 44, 37, 32, 11, 3]:
+                monkeypatch.setattr(Clock, 'now', lambda: current_time - timedelta(days=days_ago))
+                AdRecordService.add(
+                    business_id=business.id,
+                    ad_post_url=TestAdRecordService.AD_POST_URL,
+                    creator_id=user_ad_manager.id,
+                )
+            monkeypatch.undo()
+
+        @pytest.mark.parametrize('since_days_ago, include_days_report, expected_count', [
+            (0, True, 0),
+            (1, True, 0),
+            (2, True, 0),
+            (3, False, 0),
+            (3, True, 1),
+            (10, True, 1),
+            (11, True, 2),
+            (38, False, 4),
+            (70, True, 7),
+            (200, False, 10),
+        ])
+        def test_returns_valid_count(self, since_days_ago: int, include_days_report: bool, expected_count: int,
+                                     business: Business):
+            since_time = datetime.utcnow() \
+                         - timedelta(days=since_days_ago) \
+                         - (timedelta(minutes=1) if include_days_report else timedelta(seconds=0))
+            actual_count = AdRecordService.get_count_for_business_since_date(business_id=business.id, since=since_time)
+            assert actual_count == expected_count
+
+        def test_businesses_are_isolated(self, business: Business, user_admin: User, user_ad_manager: User):
+            other_business = BusinessService.add(name='Awesome carpet cleaners', facebook_url=None,
+                                                 creator_id=user_admin.id)
+            for i in range(3):
+                AdRecordService.add(
+                    business_id=business.id,
+                    ad_post_url=TestAdRecordService.AD_POST_URL,
+                    creator_id=user_ad_manager.id,
+                )
+            for i in range(2):
+                AdRecordService.add(
+                    business_id=other_business.id,
+                    ad_post_url=TestAdRecordService.AD_POST_URL,
+                    creator_id=user_ad_manager.id,
+                )
+
+            since_time = datetime.utcnow() - timedelta(minutes=1)
+            actual_count_first = AdRecordService.get_count_for_business_since_date(business_id=business.id,
+                                                                                   since=since_time)
+            actual_count_second = AdRecordService.get_count_for_business_since_date(business_id=other_business.id,
+                                                                                    since=since_time)
+            assert actual_count_first == 3
+            assert actual_count_second == 2
