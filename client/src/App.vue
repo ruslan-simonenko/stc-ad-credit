119bed7cdec776ac8d519123068f658d1687b2ed
Ruslan Simonenko <ruslan.simonenko@gmail.com>
2023-08-22 19:33:32 +0100

    client: keeping last 20 components alive

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,11 +2,21 @@
   <q-layout>
     <Header></Header>
     <q-page-container>
-      <router-view></router-view>
+      <router-view v-slot="{ Component }">
+        <keep-alive max="20" :key="userID">
+          <component :is="Component"/>
+        </keep-alive>
+      </router-view>
     </q-page-container>
   </q-layout>
 </template>
 
 <script setup lang="ts">
 import Header from "./navigation/Header.vue";
+import {computed} from "vue";
+import {useAuthStore} from "./auth/auth-store.ts";
+
+const authStore = useAuthStore();
+
+const userID = computed(() => authStore.user?.id ?? 0)
 </script>
